85fd44dfda3dbe8711b7cc4bc00ce42ed65a2502
Westigrannik <timvovk5th@gmail.com>
2026-09-07 12:57:43 +0300

    learn dart functions

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,6 +1,14 @@
-void main() {
-  List<String> fruits2 = ['z', 'b', 'u'];
-  for (var fruit in fruits2) {
-    print(fruit);
-  }
+void describePet({required String name, String species = 'кот', int age = 0}){
+  print('$name - $species, возраст $age');
 }
+
+String repeat(String text, [int times = 2]){
+  String result = '';
+  for (int i = 0; i < times; i++){
+    result += text;
+  }
+  return result;
+}
+
+void main() {
+  print(repeat('ха'));}
