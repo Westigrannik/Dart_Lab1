6bd144a691d71b8ebea335a132ff8a6230a0c151
Westigrannik <timvovk5th@gmail.com>
2026-09-07 13:01:51 +0300

    learn dart cycles

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,14 +1,12 @@
-void describePet({required String name, String species = 'кот', int age = 0}){
-  print('$name - $species, возраст $age');
-}
-
-String repeat(String text, [int times = 2]){
-  String result = '';
-  for (int i = 0; i < times; i++){
-    result += text;
-  }
-  return result;
-}
-
 void main() {
-  print(repeat('ха'));}
+  int score2 = 85;
+  String grade;
+  if (score2 >= 90) {
+    grade = 'A';
+  }  else if (score2 >= 75) {
+    grade = 'B';
+  } else{
+    grade = 'C';
+}
+print(grade);
+}
